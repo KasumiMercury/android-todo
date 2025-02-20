0ad1da1163e9f06d57faa26ad8c59ab936ec8662
KasumiMercury <naoki_shira@outlook.jp>
2025-02-20 22:49:29 +0900

    feat: TODO List item

--- app/src/main/java/com/example/androidtodo/MainActivity.kt
+++ app/src/main/java/com/example/androidtodo/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.androidtodo
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidtodo.ui.theme.AndroidTodoTheme
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +36,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun TodoListItem(title: String, deadline: LocalDateTime, modifier: Modifier = Modifier) {
+    val locale = Locale.getDefault()
+    val pattern = DateFormat.getBestDateTimePattern(locale, "yyyy/MM/dd HH:mm")
+    ListItem(
+        headlineContent = { Text(title) },
+        supportingContent = {
+            Text(
+                text = deadline.format(DateTimeFormatter.ofPattern(pattern).withLocale(locale)),
+                modifier = modifier
+            )
+        },
+    )
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -42,6 +62,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AndroidTodoTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        TodoListItem("Title", LocalDateTime.now())
     }
 }
