b6bd7476464031971429b5fa0b81cc589395532b
KasumiMercury <naoki_shira@outlook.jp>
2025-02-21 23:43:59 +0900

    feat: scrollable list

--- app/src/main/java/com/example/androidtodo/MainActivity.kt
+++ app/src/main/java/com/example/androidtodo/MainActivity.kt
@@ -5,10 +5,13 @@ import android.text.format.DateFormat
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,16 +25,32 @@ import java.util.Locale
 import java.util.UUID.randomUUID
 
 class MainActivity : ComponentActivity() {
+
+    val todos = listOf(
+        TodoItem("Buy milk", LocalDateTime.now().plusDays(1)),
+        TodoItem("Walk the dog", LocalDateTime.now().plusDays(2)),
+        TodoItem("Do homework", LocalDateTime.now().plusDays(3)),
+        TodoItem("Go to the gym", LocalDateTime.now().plusDays(4)),
+        TodoItem("Call mom", LocalDateTime.now().plusDays(5)),
+        TodoItem("Buy a present", LocalDateTime.now().plusDays(6)),
+        TodoItem("Read a book", LocalDateTime.now().plusDays(7)),
+        TodoItem("Go to the cinema", LocalDateTime.now().plusDays(8)),
+        TodoItem("Cook dinner", LocalDateTime.now().plusDays(9)),
+        TodoItem("Go to bed", LocalDateTime.now().plusDays(10)),
+        TodoItem("Wake up", LocalDateTime.now().plusDays(11)),
+        TodoItem("Go to work", LocalDateTime.now().plusDays(12)),
+        TodoItem("Go to the doctor", LocalDateTime.now().plusDays(13)),
+        TodoItem("Go to the dentist", LocalDateTime.now().plusDays(14)),
+        TodoItem("Go to the pharmacy", LocalDateTime.now().plusDays(15)),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidTodoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TodoList(todos = todos, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -65,9 +84,13 @@ fun TodoListItem(todo: TodoItem, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TodoList(todos: List<TodoItem> = emptyList()) {
-    LazyColumn {
-        items(todos, key = { todo -> todo.id() }) { todo ->
+fun TodoList(todos: List<TodoItem> = emptyList(), modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        for (todo in todos) {
             TodoListItem(todo = todo)
         }
     }
@@ -87,7 +110,19 @@ fun GreetingPreview() {
     val todos = listOf(
         TodoItem("Buy milk", LocalDateTime.now().plusDays(1)),
         TodoItem("Walk the dog", LocalDateTime.now().plusDays(2)),
-        TodoItem("Do homework", LocalDateTime.now().plusDays(3))
+        TodoItem("Do homework", LocalDateTime.now().plusDays(3)),
+        TodoItem("Go to the gym", LocalDateTime.now().plusDays(4)),
+        TodoItem("Call mom", LocalDateTime.now().plusDays(5)),
+        TodoItem("Buy a present", LocalDateTime.now().plusDays(6)),
+        TodoItem("Read a book", LocalDateTime.now().plusDays(7)),
+        TodoItem("Go to the cinema", LocalDateTime.now().plusDays(8)),
+        TodoItem("Cook dinner", LocalDateTime.now().plusDays(9)),
+        TodoItem("Go to bed", LocalDateTime.now().plusDays(10)),
+        TodoItem("Wake up", LocalDateTime.now().plusDays(11)),
+        TodoItem("Go to work", LocalDateTime.now().plusDays(12)),
+        TodoItem("Go to the doctor", LocalDateTime.now().plusDays(13)),
+        TodoItem("Go to the dentist", LocalDateTime.now().plusDays(14)),
+        TodoItem("Go to the pharmacy", LocalDateTime.now().plusDays(15)),
     )
 
     AndroidTodoTheme {
